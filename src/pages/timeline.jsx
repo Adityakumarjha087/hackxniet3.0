2be7a95df30c6c6f084cde9cefbf8370e5ffef64
Adityakumarjha087 <9aadityakumar12@gmail.com>
2025-05-06 00:12:24 +0530

    adi

--- src/pages/timeline.jsx
+++ src/pages/timeline.jsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import styles from '../styles/Timeline.module.css';
+import styles from './timeline.module.css';
 
 const Timeline = () => {
   const events = [
