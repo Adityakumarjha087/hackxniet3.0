613f23eb49f6e77093c4a8480dffc562406b468b
Adityakumarjha087 <9aadityakumar12@gmail.com>
2025-05-13 08:26:19 +0530

    doo7n1e

--- src/pages/team.js
+++ src/pages/team.js
@@ -24,7 +24,15 @@ const teamMembers = [
 
 export default function Team() {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} style={{
+      backgroundImage: "url('/images/bg57.png')",
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      minHeight: '100vh',
+      width: '100%',
+      position: 'relative'
+    }}>
       <h2 className={styles.heading}>Our Coordinator</h2>
       <div className={styles.teamGrid}>
         {teamMembers.map((member, index) => (
